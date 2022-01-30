0b06aea1793bdabb6a454e0dde4149c96ffeaeee
Ranul Navojith <ranulnavojith.99@gmail.com>
2022-01-30 18:19:26 +0530

    added JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,25 +2,38 @@ const Admin = require("../models/admin")
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
 const { ackResponse, errorResponse, successResponse } = require("../shared/responses")
+const jwt = require("jsonwebtoken")
+const accessTokenSecret = 'youraccesstokensecret'
+
 // Load input validation
 const validateRegisterInput = require("../validation/signUp");
 // const validateLoginInput = require("../../validation/login");
+
 exports.adminSignIn = function (req, res) {
     // var email = req.body.email;
     // var password = req.body.password;
-    var {email,password} = req.body
+    var { email, password } = req.body
 
-    Admin.findOne({ email:email }).then(admin => {
-        if(admin){
+    Admin.findOne({ email: email }).then(admin => {
+        if (admin) {
             const cmp = bcrypt.compareSync(password, admin.password);
-            if(cmp){
-                successResponse(res, 'Admin Login successful', admin);
+            if (cmp) {
+                const token = jwt.sign({email: admin.email, nic: admin.nic},accessTokenSecret, {expiresIn: "5min"})
+                // localStorage.setItem('token', token)
+                console.log(token)
+                const adminData = {
+                    name : admin.firstName + ' ' + admin.lastName,
+                    email: admin.email,
+                    token: token,
+                    nic : admin.nic
+                }
+                successResponse(res, 'Admin Login successful',adminData);
+
             }
-            else{
+            else {
                 errorResponse(res, null, 'Invalid Password', null);
             }
-            
-        }else{
+        } else {
             errorResponse(res, 404, 'Admin not found', null);
         }
     }).catch(err => {
@@ -29,23 +42,19 @@ exports.adminSignIn = function (req, res) {
 }
 
 //user auth controller | signin
-
 exports.userSignIn = function (req, res) {
-    // var email = req.body.email;
-    // var password = req.body.password;
-    var {email,password} = req.body
+    var { email, password } = req.body
 
-    User.findOne({ email:email }).then(user => {
-        if(user){
+    User.findOne({ email: email }).then(user => {
+        if (user) {
             const cmp = bcrypt.compareSync(password, user.password);
-            if(cmp){
+            if (cmp) {
                 successResponse(res, 'User Login successful', user);
             }
-            else{
+            else {
                 errorResponse(res, null, 'Invalid Password', null);
             }
-            
-        }else{
+        } else {
             errorResponse(res, 404, 'User not found', null);
         }
     }).catch(err => {
@@ -56,7 +65,7 @@ exports.userSignIn = function (req, res) {
 //user auth controller | signup
 
 // exports.userSignUp = async(req,res) =>{
-    // const {firstName, lastName, nic, phoneNumber,email,password } = req.body;
+// const {firstName, lastName, nic, phoneNumber,email,password } = req.body;
 
 //     try{
 //         const user = User.create({
@@ -71,38 +80,38 @@ exports.userSignIn = function (req, res) {
 // }
 
 //user auth controller | signup 2
-exports.userSignUp=function(req, res){
-// Form validation
-  const { errors, isValid } = validateRegisterInput(req.body);
-// Check validation
+exports.userSignUp = function (req, res) {
+    // Form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+    // Check validation
     if (!isValid) {
-    return res.status(400).json(errors);
+        return res.status(400).json(errors);
     }
-    const {firstName, lastName, nic, phoneNumber,email,password } = req.body;
+    const { firstName, lastName, nic, phoneNumber, email, password } = req.body;
 
-  User.findOne({ email}||{nic}).then(user => {
-      if (user) {
-        return res.status(400).json({ email: "Email or NIC already exists" });
-      } else {
-        const newUser = new User({
-          firstName,
-          lastName,
-          nic,
-          phoneNumber,
-          email,
-          password
-        });
-  // Hash password before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
-      }
+    User.findOne({ email } || { nic }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email or NIC already exists" });
+        } else {
+            const newUser = new User({
+                firstName,
+                lastName,
+                nic,
+                phoneNumber,
+                email,
+                password
+            });
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
     });
-  };
+};
