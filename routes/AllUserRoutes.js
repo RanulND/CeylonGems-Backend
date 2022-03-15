ba9b0e8f57bf825cf1318bbba7d8136e2a21e5b9
Fathima Husna <husnassf.19@uom.lk>
2022-03-15 20:52:02 +0530

    minor changes

--- routes/AllUserRoutes.js
+++ routes/AllUserRoutes.js
@@ -8,6 +8,6 @@ const User = require('../models/user');
 //importing...
 const AllUserController = require('../controllers/AllUserController')
 
-router.post('/getallusers', AllUserController.getAllUsers);
+//router.post('/getallusers', AllUserController.getAllUsers);
 
 module.exports = router;
