24956245b56d59ff8ca3cb5bfa0e071d5201c946
Hashan Chathuranga <chathurangabkh.19@itfac.mrt.ac.lk>
2022-05-10 16:56:12 +0530

    Added the cart model and controllers and routes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const AuthRoutes = require('./authRoutes');
 const GemRoutes = require('./gemRoutes');
 const AdminRoutes = require('./adminRoutes');
 const JewelleryRoutes = require('./jewelleryRoutes');
+const CartRoutes = require('./cartRoutes');
 
 router.use('/auth', AuthRoutes);
 router.use('/gem', GemRoutes);
 router.use('/admin', AdminRoutes);
 router.use('/jewellery',JewelleryRoutes);
+router.use('/cart', CartRoutes )
 
 module.exports = router;
