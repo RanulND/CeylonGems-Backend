f79f3de1480cd8ea5bb717dddf851553baf014e9
Hashan Chathuranga <chathurangabkh.19@itfac.mrt.ac.lk>
2022-05-23 23:29:07 +0530

    added the cart functions

--- models/order.js
+++ models/order.js
@@ -1,9 +1,61 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-const orderSchema = {
 
-}
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    itemList: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Gem' || 'Jewellery'
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            },
+        }
+    ],
+        country: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String
+        },
+        streetAddress: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        province: {
+            type: String,
+            required: true,
+        },
+        zipCode: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        orderValue: {
+            type: String,
+            required: true
+        }
+
+    }, { timestamps: true });
 
 const Order = mongoose.model('Order', orderSchema, 'order');
 module.exports = Order;
