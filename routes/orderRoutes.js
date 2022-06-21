709d34bbf44cfb49d4c98b42ac1f853409280b77
NelmiEK99 <nelmikudagodage@gmail.com>
2022-06-21 14:28:43 +0530

    minor change

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,9 +5,4 @@ const orderController = require('../controllers/OrderController');
 
 router.post('/addOrder', orderController.addOrder);
 
-router.post('/productdetails', orderController.getProductDetails);
-router.get('/order/:id', orderController.getOrder);
-router.post('/getorderbyid', orderController.getOrderDetails);
-router.delete('/deleteorder', orderController.deleteOrder);
-
 module.exports = router;
